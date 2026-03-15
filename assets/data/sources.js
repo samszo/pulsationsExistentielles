16fd1dd9ac2fc574c427cb94e076f0f5a6360a2c
samszo <samszon@gmail.com>
2026-03-15 10:16:19 +0100

    Ajout de nouvelles ressources dans sources.js et mise à jour des permissions du fichier

--- assets/data/sources.js
+++ assets/data/sources.js
@@ -1,5 +1,19 @@
 [
     {
+        "name": "M2 ACEHN",
+        "url": "https://acehn.jardindesconnaissances.fr/api/items?resource_class_id=509&per_page=10000",
+        "fct":"genStory",
+        "value":"s_0",
+        "children": []
+    },
+    {
+        "name": "64 propositions pour une éthique des systèmes d’intelligence artificielle",
+        "url": "https://acehn.jardindesconnaissances.fr/admin/item?property[0][joiner]=and&property[0][property]=33&property[0][type]=res&property[0][text]=64859&per_page=10000",
+        "fct":"genStory",
+        "value":"s_0",
+        "children": []
+    },
+    {
         "name": "Creation P8",
         "url": "http://localhost/omk_creationsp8/api/items?resource_class_id=1314",
         "fct":"genStory",
